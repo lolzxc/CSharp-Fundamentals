a7828b85289e6e7e17b903e74b02c1703f45b787
lolzxc <guitarbeginner2@gmail.com>
2023-10-09 08:17:49 +0800

    ArrayList Example

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Collections
+﻿using System.Collections;
+
+namespace Collections
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ArrayListExample arrayListExample = new ArrayListExample();
+        }
+    }
+
+    public class ArrayListExample
+    {
+        public ArrayListExample()
+        {
+            ArrayList strArray = new ArrayList();
+            strArray.AddRange(new String[] { "First", "Second", "Third " });
+
+            Console.WriteLine("This collection has {0} items.", strArray.Count);
+            Console.WriteLine();
+
+            strArray.Add("Fourth");
+            Console.WriteLine("This collection has {0} items.", strArray.Count);
+
+            foreach (string s in strArray)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine();
         }
     }
 }
