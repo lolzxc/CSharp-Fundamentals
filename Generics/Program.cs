b8274650d7da28e00f9aa86bbe536fe28c273b42
lolzxc <guitarbeginner2@gmail.com>
2023-10-10 18:34:34 +0800

    Ch 10

---
+++ Generics/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            UseGenericList();
+        }
+        
+        static void UseGenericList()
+        {
+            Console.WriteLine("Generics");
+
+            List<Person> morePeople = new List<Person>();
+            morePeople.Add(new Person("Jim", "Garrido", 23));
+            Console.WriteLine(morePeople[0]);
+
+        }
+    }
+    public class Person
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person()
+        {
+
+        }
+        public Person (string firstName, string lastName, int age)
+        {
+            Age = age;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName}, Age: {Age}";
+        }
+    }
+}
---
+++ IssueWithNonGenericCollections/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+namespace IssueWithNonGenericCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            WorkingWithArrayList(); 
+        }
+        
+        static void WorkingWithArrayList()
+        {
+            ArrayList myInts = new ArrayList();
+            myInts.Add(1);
+            myInts.Add(2);
+            myInts.Add(3);
+
+            int i = (int)myInts[0];
+
+            Console.WriteLine("Value of your int: {0}", i);
+        }
+    }
+}
